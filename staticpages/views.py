041bff90cd99e8c230f22996c396618a3bb0892d
Felipe <fefeijo04@gmail.com>
2024-09-23 16:07:04 -0300

    commit final

--- staticpages/views.py
+++ staticpages/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index (request):
-  return HttpResponse("<h1>Hello World!</h1>")
+  context = {}
+  return render(request, 'staticpages/index.html', context)
+
+def about (request):
+  context = {}
+  return render(request, 'staticpages/about.html', context)
